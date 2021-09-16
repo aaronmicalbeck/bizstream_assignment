e8f4f3a5c866fd5be700edbe1d0193ee422e6cf7
Aaron Beck <aaronmicalbeck@gmail.com>
2021-09-15 18:18:16 -0700

    restructuring files

---
+++ app.js
@@ -0,0 +1,3 @@
+console.log("Thank you for considering me for the Full Stack Web Developer position.  I look forward to hearing from you soon.  -Aaron Beck");
+
+
---
+++ models/Contacts.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema ({   
+    
+                        firstName:{ type: String, required: true, min: 1, max: 50 },
+                        lastName: { type: String, required: true, min: 1, max: 50 },
+                        email:    { type: String, required: true, min: 3, max: 50 },
+                        message:  { type: String, required: true, min: 1, max: 250 }
+                    })
+
+  
+
+    module.exports = mongoose.model('Contact', contactSchema);
